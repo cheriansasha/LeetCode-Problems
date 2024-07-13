91eb456d7ca105cf0c7ee206f8b1a604ca1b8a50
Sasha Cherian <151881634+cheriansasha@users.noreply.github.com>
2024-07-13 17:57:55 -0400

    Time: 1 ms (86.63%), Space: 40.7 MB (40.8%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int mySqrt(int x) {
+        //understand
+
+        //match
+        //binary search
+
+        //plan
+        //initalize base case, start, middle, and end ints
+        //find number based on binary search
+
+        //implement
+        if(x == 0 || x == 1) {
+            return x;
+        }
+        int start = 1;
+        int mid = -1;
+        int end = x;
+
+        //keep going until cant keep looking on left or right side of range
+        while(start <= end) {
+            mid = start + ((end - start) / 2);
+            //we need to search for smaller numbers
+            //gotta keep long to keep real value in place
+            if((long) mid * (long) mid > x) {
+                end = mid - 1;
+            }
+            else if(mid * mid == x) {
+                return mid;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return Math.round(end);
+    }
+}
