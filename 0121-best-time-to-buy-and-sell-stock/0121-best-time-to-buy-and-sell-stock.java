4869fa1c268c8ebae255e5f5162106745a3b6516
Sasha Cherian <151881634+cheriansasha@users.noreply.github.com>
2024-08-08 10:35:44 -0400

    Time: 3 ms (7.91%), Space: 57.4 MB (97.83%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,35 +1,24 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        //main idea: to check for min costs while also look for 
+        //max profit at every day
+
+        //base case
         if(prices.length == 1) {
             return 0;
         }
 
-        // int maxProfit = 0;
-        // int tempCost = 0;
-
-        // for(int i = 0; i < prices.length; i++) {
-        //     for(int j = i + 1; j < prices.length; j++) {
-        //         tempCost = prices[j] - prices[i];
-        //         if(tempCost > maxProfit) {
-        //             maxProfit = tempCost;
-        //         }
-        //     }
-        // }
-
-        // return maxProfit;
-        
-
-        //more time efficient solution
-        //check minCost while also checking for maxProfit
-        int minCostArr[] = new int[prices.length + 1];
+        int minCost[] = new int[prices.length + 1];
         int maxProfit = 0;
-        minCostArr[0] = prices[0];
 
-        for(int i = 0; i < prices.length; i++) {
-            minCostArr[i + 1] = Math.min(minCostArr[i], prices[i]);
-            maxProfit = Math.max(maxProfit, (prices[i] - minCostArr[i]));
+        minCost[0] = prices[0];
+        minCost[1] = prices[1];
+
+        for(int i = 0; i < minCost.length - 1; i++) {
+            minCost[i + 1] = Math.min(minCost[i], prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - minCost[i]);
         }
-        
+
         return maxProfit;
     }
 }
