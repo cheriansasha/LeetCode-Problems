b4115c70578e3611f9dc7b8373e8b5ab034820ab
Sasha Cherian <151881634+cheriansasha@users.noreply.github.com>
2024-08-11 17:15:42 -0400

    Time: 0 ms (100%), Space: 66.5 MB (83.39%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,22 +1,19 @@
 class Solution {
     public int findCenter(int[][] edges) {
+        //idea: check which node is connected to 2 different edges 
+
         int node1 = edges[0][0];
         int node2 = edges[0][1];
         int node3 = edges[1][0];
         int node4 = edges[1][1];
 
-        if(node1 == node3){
+        if(node1 == node3 || node1 == node4) {
             return node1;
         }
-        if(node2 == node3){
+        if(node2 == node3 || node2 == node4) {
             return node2;
         }
-        if(node1 == node4){
-            return node4;
-        }
-        if(node2 == node4){
-            return node4;
-        }
+        
         return 0;
     }
 }
