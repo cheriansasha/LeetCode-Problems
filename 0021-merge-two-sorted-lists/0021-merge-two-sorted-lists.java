62745aa6a2d9246c42cb10cd471812afb90ae82c
Sasha Cherian <151881634+cheriansasha@users.noreply.github.com>
2025-12-16 19:17:39 -0500

    Time: 0 ms (100%), Space: 44.2 MB (53.57%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,58 +10,37 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        //edge cases
-        if(list1 == null && list2 == null) {
-            return null;
-        }
-        else if(list1 == null) {
-            return list2;
-        }
-        else if(list2 == null) {
-            return list1;
-        }
+        ListNode dummyNode = new ListNode();
+        ListNode tail = dummyNode;
 
-        ListNode curr1 = list1;
-        ListNode curr2 = list2;
-        ListNode result = new ListNode();
-        ListNode currResult;
+        while(list1 != null && list2 != null) {
 
-        //find result head
-        if(curr1.val <= curr2.val) {
-            result = curr1;
-            currResult = curr1;
-            curr1 = curr1.next;
-        }
-        else{
-            result = curr2;
-            currResult = curr2;
-            curr2 = curr2.next;
-        }
-
-        //find rest of result LL
-        while(curr1 != null && curr2 != null) {
-            if(curr1.val <= curr2.val) {
-                currResult.next = curr1;
-                curr1 = curr1.next;
+            //append list1's head to the list by pointing tail's next pointer 
+            //to list1's head
+            if(list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
             }
-            else  {
-                currResult.next = curr2;
-                curr2 = curr2.next;
+
+            //do the same if list2 is smaller than list1
+            else {
+                tail.next = list2;
+                list2 = list2.next;
             }
-            currResult = currResult.next;
+
+            //move tail pointer forward one node
+            tail = tail.next;
         }
 
-        while(curr1 != null) {
-            currResult.next = curr1;
-            curr1 = curr1.next;
-            currResult = currResult.next;
+        //append the rest of either lists if nodes left
+        if(list1 != null) {
+            tail.next = list1;
         }
-        while(curr2 != null) {
-            currResult.next = curr2;
-            curr2 = curr2.next;
-            currResult = currResult.next;
+        else {
+            tail.next = list2;
         }
 
-        return result;
+        //returns the head of the resulting list
+        return dummyNode.next;
     }
 }
